17818bff1d97054a0c4468cefda36379e5072373
marrinaanna <marinaanna542@gmail.com>
2023-04-22 12:16:33 +0300

    some fix

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -15,7 +15,7 @@ it("Should not login with empty login", () => {
     .should("be.false");
   cy.get("#mail")
     .then(($el) => $el[0].validationMessage)
-    .should("contain", "Заполните это поле.");
+    .should("contain", "Пожалуйста, заполните это поле.");
 });
 
 it("Should not login with empty password", () => {
